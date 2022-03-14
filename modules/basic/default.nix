52c2b7f3269be216b772002a8adc1edc66ba7cb1
Christian Harke <christian.harke@bluecare.ch>
2022-03-14 20:30:11 +0100

    Add window split keybindings

--- modules/basic/default.nix
+++ modules/basic/default.nix
@@ -238,6 +238,26 @@ in
         set termguicolors
         set t_Co=256
       ''}
+
+      " Open terminal inside vim
+      map <Leader>tt :split<bar>terminal<CR>
+      map <Leader>tv :vsplit<bar>terminal<CR>
+
+      " Remap splits navigation to just CTRL + hjkl
+      nnoremap <C-h> <C-w>h
+      nnoremap <C-j> <C-w>j
+      nnoremap <C-k> <C-w>k
+      nnoremap <C-l> <C-w>l
+
+      " Make adjusing split sizes a bit more friendly
+      noremap <silent> <C-Left> :vertical resize +3<CR>
+      noremap <silent> <C-Right> :vertical resize -3<CR>
+      noremap <silent> <C-Up> :resize +3<CR>
+      noremap <silent> <C-Down> :resize -3<CR>
+
+      " Change 2 split windows from vert to horiz or horiz to vert
+      map <Leader>th <C-w>t<C-w>H
+      map <Leader>tk <C-w>t<C-w>K
     '';
   };
 }
